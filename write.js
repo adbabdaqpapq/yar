d7c614ce9036f8ad38ffe95d3c28e60d8a5f6ac2
adbabdaqpapq <limungyeol@gmail.com>
2026-08-21 13:30:20 +0900

    Add files via upload

--- write.js
+++ write.js
@@ -1,1 +1,21 @@
+const postButton = document.getElementById("post-button");
 
+postButton.addEventListener("click", function() {
+    const title = document.getElementById("post-title").value;
+    const content = document.getElementById("post-content").value;
+    if (title === "" || content === "") {
+        alert("제목과 내용을 모두 입력해주세요.");
+        return;
+                }
+
+        const post = {
+        title: title,
+        content: content
+                };
+
+        localStorage.setItem("post", JSON.stringify(post));
+
+        alert("게시글이 등록되었습니다!");
+
+        window.location.href = "index.html";
+        });
